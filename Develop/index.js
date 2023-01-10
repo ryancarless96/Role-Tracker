4266f801f3b1c525e9723ad3da5f4db69411c131
Ryan Carless <ryancarless96@gmail.com>
2023-01-10 13:56:08 -0500

    commit changes

--- Develop/index.js
+++ Develop/index.js
@@ -16,7 +16,7 @@ const db = mysql.createConnection(
     // MySQL username,
     user: "root",
     // TODO: Add MySQL password here
-    password: "",
+    password: "password",
     database: "employees_db",
   },
   console.log(`Connected to the employees_db database.`)
