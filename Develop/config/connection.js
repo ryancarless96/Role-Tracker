3a16326f8323424d08399383917d46add2b68ae3
Ryan Carless <ryancarless96@gmail.com>
2023-01-27 11:43:52 -0500

    commit changes

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,27 +1,15 @@
 const mysql = require("mysql2");
 const connection = mysql.createConnection({
-
     host: "localhost",
-  
     // Your username
-    //hair
-  
     user: "root",
-  
     // Your password
-  
     password: "password",
-  
     database: "employees_db"
   
   });
-  
-  
-  
   connection.connect(function (err) {
-  
     if (err) throw err;
-  
   });
   
   
